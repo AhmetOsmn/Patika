c5c0b6b096a228fd562b5fc7f16f7cbc1759e1a6
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-02-01 23:36:03 +0300

    Karar yapıları tamamlandı

--- BaslangicSeviyesiDotnetCorePatikasi/Pratikler/06.KararYapilari/Program.cs
+++ BaslangicSeviyesiDotnetCorePatikasi/Pratikler/06.KararYapilari/Program.cs
@@ -8,7 +8,8 @@ namespace _06.KararYapilari
         {
             int time = DateTime.Now.Hour;
 
-            if(time >= 6 && time < 11){
+            if (time >= 6 && time < 11)
+            {
                 System.Console.WriteLine("Günaydın");
             }
             else if (time <= 18)
@@ -22,8 +23,41 @@ namespace _06.KararYapilari
 
             string sonuc = time <= 18 ? "İyi günler" : "İyi geceler";
             string sonuc2 = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+            System.Console.WriteLine("-----------IF-ELSE-----------");
             System.Console.WriteLine("Sonuc1: " + sonuc);
             System.Console.WriteLine("Sonuc2: " + sonuc2);
+
+            int month = DateTime.Now.Month;
+            System.Console.WriteLine("-----------SWITCH-CASE-----------");
+            switch(month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    System.Console.WriteLine("Kış");
+                    break;
+    
+                case 3:       
+                case 4:
+                case 5:
+                    System.Console.WriteLine("İlkbahar");
+                    break;
+                
+                case 6:               
+                case 7:               
+                case 8:
+                    System.Console.WriteLine("Yaz");
+                    break;
+               
+                case 9: 
+                case 10:
+                case 11:
+                    System.Console.WriteLine("Sonbahar");
+                    break;
+
+                default:
+                break;
+            }
         }
     }
 }
