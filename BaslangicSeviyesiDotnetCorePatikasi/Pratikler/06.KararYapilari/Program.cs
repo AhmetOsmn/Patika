4a28d1ab78fb8b05bd4a72762733e214b84c7136
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-02-01 23:12:30 +0300

    Kontrol Yapılarp P1

---
+++ BaslangicSeviyesiDotnetCorePatikasi/Pratikler/06.KararYapilari/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06.KararYapilari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time >= 6 && time < 11){
+                System.Console.WriteLine("Günaydın");
+            }
+            else if (time <= 18)
+            {
+                System.Console.WriteLine("İyi günler");
+            }
+            else
+            {
+                System.Console.WriteLine("İyi geceler");
+            }
+
+            string sonuc = time <= 18 ? "İyi günler" : "İyi geceler";
+            string sonuc2 = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+            System.Console.WriteLine("Sonuc1: " + sonuc);
+            System.Console.WriteLine("Sonuc2: " + sonuc2);
+        }
+    }
+}
