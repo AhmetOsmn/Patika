8a9818cc618c441b32f54baf73fbab3bcb4165cf
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-02-02 00:07:52 +0300

    Donguler tamamlandı

--- BaslangicSeviyesiDotnetCorePatikasi/Pratikler/07.Donguler/Program.cs
+++ BaslangicSeviyesiDotnetCorePatikasi/Pratikler/07.Donguler/Program.cs
@@ -18,25 +18,54 @@ namespace _07.Donguler
             // }
 
             // 1-1000 arasındaki tek ve çift sayıların toplamı
-            int tekToplam = 0;
-            int ciftToplam = 0;
-            for (int i = 1; i <= 1000; i++)
-            {
-                if (i % 2 == 1)
-                {
-                    tekToplam += i;
-                }
-                else
-                {
-                    ciftToplam += i;
-                }
-            }
-
-            System.Console.WriteLine("Cift toplam: " + ciftToplam);
-            System.Console.WriteLine("Tek toplam: " + tekToplam);
+            // int tekToplam = 0;
+            // int ciftToplam = 0;
+            // for (int i = 1; i <= 1000; i++)
+            // {
+            //     if (i % 2 == 1)
+            //     {
+            //         tekToplam += i;
+            //     }
+            //     else
+            //     {
+            //         ciftToplam += i;
+            //     }
+            // }
+            // System.Console.WriteLine("Cift toplam: " + ciftToplam);
+            // System.Console.WriteLine("Tek toplam: " + tekToplam);
 
             // Eğer döngüyü kırmak istiyorsak break kullanırız. Sadece 1 döngüyü kırar. İç içe döngülerde sadece bulunduğu döngüyü kırar.
             // Eğer bir bloğun çalışmasını istemiyorsak, bloğun üst kısmında continue kullanırız.
+
+            // while
+            // 1'den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp ekrana yazdıran program.
+            // System.Console.WriteLine("Lutfen bir sayı giriniz: ");
+            // int sayi = int.Parse(Console.ReadLine());
+            // int sayac = 1;
+            // float toplam = 0;
+            // while(sayac <= sayi){
+            //     toplam += sayac;
+            //     sayac++; 
+            // }
+            // System.Console.WriteLine("Ortalama: " + (toplam/sayi));
+
+            // 'a' dan 'z' ye kadar tüm harfleri ekrana yazdır.
+            // char karakter = 'a';
+            // while(karakter <= 'z'){
+            //     System.Console.Write(karakter + " - ");
+            //     karakter++;
+            // }
+
+            // Foreach
+            string[] arabalar = {"BMW","Ford", "Mercedes","Toyota"};
+            foreach (var araba in arabalar)
+            {
+                System.Console.WriteLine("Araba: " + araba);
+            }
+
+
+
+
         }
     }
 }
