41630dfb2c6c4d9fdc9726f59ef8fec8fe86020d
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-02-02 20:48:42 +0300

    Soru4 tamamlandı

---
+++ BaslangicSeviyesiDotnetCorePatikasi/Pratikler/Odev1/Soru4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Soru4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz: ");
+
+            string cumle = Console.ReadLine();
+            string[] kelimeler = cumle.Split(" ");
+
+            int toplamKelime = kelimeler.Length;
+            int toplamHarf = 0;
+
+            foreach (var item in kelimeler)
+            {
+                toplamHarf += item.Length;
+            }
+
+            System.Console.WriteLine("Kelime sayısı: {0}, harf sayısı: {1}",toplamKelime,toplamHarf);
+        }
+    }
+}
