983d85a64a812ac3a001f5e250c9ab37ecc6bc50
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-02-02 20:41:06 +0300

    Soru3 tamamlandı

---
+++ BaslangicSeviyesiDotnetCorePatikasi/Pratikler/Odev1/Soru3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Soru3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozitif bir tam sayı girin (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            string[] kelimeler = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.WriteLine("{0} kelimeden {1}. kelimeyi giriniz: ",n,i+1);
+                kelimeler[i] = Console.ReadLine();
+            }
+
+            Array.Reverse(kelimeler);
+            System.Console.WriteLine("Girilen kelimeler sondan başa doğru:");
+            foreach (var item in kelimeler)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
