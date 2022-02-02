e2ccfc9d4ef1061334a29c2436ae215659f6f170
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-02-02 20:16:46 +0300

    Soru1 tamamlandı

---
+++ BaslangicSeviyesiDotnetCorePatikasi/Pratikler/Odev1/Soru1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Soru1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen dizi boyutunu girin (n): ");
+            int size = int.Parse(Console.ReadLine());
+            int[] sayilar = new int[size];
+            if(size > 0){
+                for (int i = 0; i < size; i++)
+                {
+                    System.Console.Write("Sayi giriniz: ");
+                    sayilar[i] = int.Parse(Console.ReadLine());
+                }
+            }
+            else {
+                System.Console.WriteLine("Geçersiz dizi boyutu girdiniz. Boyut pozitif olmalı");
+            }
+
+            string result = "";
+
+            foreach (var item in sayilar)
+            {
+                if(item % 2 == 0){
+                    result += item + " ";
+                }
+            }
+            System.Console.WriteLine("Girilen çift sayılar: " + result);
+        }
+    }
+}
