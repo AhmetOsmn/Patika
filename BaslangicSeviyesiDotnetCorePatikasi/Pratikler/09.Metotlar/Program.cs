e7278e6786930c31f275f2fe5aba40a2d7caccff
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-02-02 19:35:28 +0300

    Metotlar P2

--- BaslangicSeviyesiDotnetCorePatikasi/Pratikler/09.Metotlar/Program.cs
+++ BaslangicSeviyesiDotnetCorePatikasi/Pratikler/09.Metotlar/Program.cs
@@ -6,6 +6,40 @@ namespace _09.Metotlar
     {
         static void Main(string[] args)
         {
+            Metotlar metotlar = new Metotlar();
+
+            // Metotlar1();
+
+            // out parametreler
+            string sayi = "999";
+            int outSayi;
+
+            bool sonuc = int.TryParse(sayi, out outSayi);
+
+            if (sonuc)
+            {
+                System.Console.WriteLine("Başarılı!");
+                System.Console.WriteLine(outSayi);
+            }
+            else
+            {
+                System.Console.WriteLine();
+            }
+
+            metotlar.Topla(2, 3, out int toplamSonuc);
+            System.Console.WriteLine(toplamSonuc);
+
+            //Metot aşırı yükleme - Overloading
+            int ifade = 123;
+            metotlar.EkranaYazdir(ifade.ToString());
+            metotlar.EkranaYazdir(ifade);
+
+        }
+
+
+
+        static void Metotlar1(Metotlar metotlar)
+        {
             // erisim_belirteci geri_donus_tipi metot_adı(parametre_listesi){
             //     komutlar
             //     return;
@@ -13,9 +47,8 @@ namespace _09.Metotlar
             int a = 2;
             int b = 3;
             System.Console.WriteLine("Klasik: " + (a + b));
-            System.Console.WriteLine("Topla: " + Topla(a, b));
+            System.Console.WriteLine("Topla: " + metotlar.Topla(a, b));
 
-            Metotlar metotlar = new Metotlar();
             metotlar.EkranaYazdir((a + b).ToString());
             System.Console.WriteLine("klasik -- a: {0}, b: {1}, toplam: {2}", a, b, a + b);
 
@@ -24,20 +57,29 @@ namespace _09.Metotlar
 
             System.Console.WriteLine("metot --a: {0}, b: {1}, toplam: {2}", a, b, metotlar.ArttirVeTopla2(ref a, ref b));
             System.Console.WriteLine("a: {0}, b: {1}", a, b);
-
-        }
-
-        static int Topla(int deger1, int deger2)
-        {
-            return (deger1 + deger2);
         }
     }
 
     class Metotlar
     {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+
         public void EkranaYazdir(string veri)
         {
             System.Console.WriteLine("EkranaYazdir: " + veri);
+        }
+
+        public void EkranaYazdir(int veri)
+        {
+            System.Console.WriteLine("EkranaYazdir2: " + veri);
         }
 
         public int ArttirVeTopla(int deger1, int deger2)
