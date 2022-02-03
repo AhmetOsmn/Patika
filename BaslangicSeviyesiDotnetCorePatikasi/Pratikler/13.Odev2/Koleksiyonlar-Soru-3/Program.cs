f78caf18c79f88e6bc4ff3ea942d440c064a1ec3
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-02-04 00:33:11 +0300

    Soru3 tamamlandı

---
+++ BaslangicSeviyesiDotnetCorePatikasi/Pratikler/13.Odev2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] sesliHarfler = {'a','e','ı','i','o','ö','u','ü'};
+            List<char> cumledekiSesliHarfler = new List<char>();
+
+            System.Console.Write("Bir cümle giriniz: ");
+            string cumle = Console.ReadLine();
+
+            foreach (var harf in cumle)
+            {
+                if(Array.IndexOf(sesliHarfler,harf) != -1){
+                    cumledekiSesliHarfler.Add(harf);
+                }
+            }
+
+            string harfler = "";
+            foreach (var item in cumledekiSesliHarfler)
+            {
+                harfler += item + " ";
+            }
+            System.Console.WriteLine("Cümledeki sesli harfler: " + harfler);
+
+        }
+    }
+}
