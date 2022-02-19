5df46f7f714821ce3f7396aa62b2336477f9e975
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-02-19 21:16:04 +0300

    Mapper onarildi

--- OrtaSeviyeDotnetCorePatikasi/02.DotnetCore/Pratikler/BookStore/WebApi/Common/MappingProfile.cs
+++ OrtaSeviyeDotnetCorePatikasi/02.DotnetCore/Pratikler/BookStore/WebApi/Common/MappingProfile.cs
@@ -17,14 +17,19 @@ namespace WebApi.Common
         public MappingProfile()
         {
             CreateMap<CreateBookModel, Book>();
-            CreateMap<Book, BookDetailViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name));
-            CreateMap<Book, BookDetailViewModel>().ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.Author.Name));
-            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name));
-            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.Author.Name));
+
+            CreateMap<Book, BookDetailViewModel>()
+                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name))
+                .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.Author.Name + " " + src.Author.Surname));
+
+            CreateMap<Book, BooksViewModel>()
+                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name))
+                .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.Author.Name + " " + src.Author.Surname));
+
 
             CreateMap<Genre, GenresViewModel>();
             CreateMap<Genre, GenreDetailViewModel>();
-            
+
             CreateMap<CreateAuthorModel, Author>();
             CreateMap<Author, AuthorsViewModel>();
             CreateMap<Author, AuthorDetailViewModel>();
